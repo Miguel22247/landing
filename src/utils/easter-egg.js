66f0d15bec8a361a09925b2d19367b7961c13079
Miguel Pacheco <minipachru@gmail.com>
2025-05-12 13:51:37 -0300

    feat: add easter-egg function

---
+++ src/utils/easter-egg.js
@@ -0,0 +1,6 @@
+export function triggerEasterEgg() {
+  console.log(
+    '%cBuscamos desarrolladores como tú, demuestra tu talento en el repositorio de GitHub :)',
+    'background: #fcaf17; color: white; padding-left: 5px; padding-right: 5px; font-size: 20px;'
+  );
+}
